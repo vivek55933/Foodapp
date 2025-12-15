283f6da4fb870151df5bd0ee0c75a1ffa7638358
vivek55933 <vivek.m.choudhary@slrtce.in>
2025-12-15 22:34:40 +0530

    message

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './Context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
- <App />
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+
   </BrowserRouter>
-   
+
 
 )
