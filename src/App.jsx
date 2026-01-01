31757d75e2b5127e7ec04e897bf425184350c339
vivek55933 <vivek.m.choudhary@slrtce.in>
2026-01-01 22:15:15 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Cart from './pages/Cart/Cart'
@@ -6,11 +6,14 @@ import Placeorder from './pages/Placeorder/Placeorder'
 import Home from './pages/Home/Home'
 import FoodDisplay from './components/FoodDisplay/FoodDisplay'
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
+  const[showLogin,setShowLogin]=useState(false);
   return (
       <>
-        <Navbar />
+      {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
+        <Navbar setShowLogin={setShowLogin}/>
          <div className='app'>
         <Routes>
           <Route path='/' element={<Home />} />
